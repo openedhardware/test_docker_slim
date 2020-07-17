d445d0d3e714be4a1df565d2e17bc523e96f3ed5
RPi Guru <rpiguru@techie.com>
2020-07-17 14:54:33 +0800

    Removing redundant modules.

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ RUN apt-get update && apt-get install -y build-essential cmake python3.7-dev
 RUN pip3 install requests
 
 WORKDIR /usr/src/app
+
 COPY . ./
 
 CMD ["python3", "main.py"]
-#CMD [ "sleep", "infinity"]
--- main.py
+++ main.py
@@ -5,16 +5,11 @@ import requests
 
 
 COCO_LABELS_URL = "https://raw.githubusercontent.com/pjreddie/darknet/master/data/coco.names"
-NODERED_URL = 'http://0.0.0.0:1880'
 
 
 class TestDockerSlim(threading.Thread):
 
     def run(self):
-        r = requests.get(f'{NODERED_URL}/flows', headers={"Node-RED-API-Version": "v2"})
-        nodered_flows = r.json()['flows']
-        print(f"Node-RED flows: {nodered_flows}")
-
         print(f"Downloading label file - {COCO_LABELS_URL}")
         with open(os.path.join('/data/', 'labels.txt'), 'wb') as f:
             r = requests.get(COCO_LABELS_URL)
