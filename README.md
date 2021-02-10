d7e668d1bfb810e61c27b9433c4302d62271f36f
RPi Guru <rpiguru@techie.com>
2021-02-10 19:21:16 +1000

    Updated to use the environment variable.

--- README.md
+++ README.md
@@ -7,14 +7,19 @@ DockerSlim testing app
 docker build -t ts .
 ```
 
+## Test original image
+```shell script
+docker run -v /data:/data --rm --network host -e MODEL='ssd_mobilenet_v1_coco_2018_01_28' ts
+```
+
 ## Slimify
 
 ```shell script
 docker-slim build --http-probe=false --mount /data:/data --include-path /usr/local/lib/python3.7/site-packages/certifi --show-clogs ts
 ```
 
-## Test to see what happens
+## Test to see what happens with the slim result image
 
 ```shell script
-docker run --privileged -v /data:/data --rm --network host ts.slim
+docker run -v /data:/data --rm --network host -e MODEL='ssd_mobilenet_v1_coco_2018_01_28' ts.slim
 ```
--- main.py
+++ main.py
@@ -1,14 +1,12 @@
+import os
 import requests
 
-r = requests.get('http://0.0.0.0:1880/flows')
-for f in r.json():
-    if f['type'] == 'object-detection':
-        print(f"Found a flow - {f}")
-        model_name = f['model_name']
-        url = "https://viso-public-storage.s3.eu-central-1.amazonaws.com/ObjDet/CPU/{}.tar.xz".format(model_name)
-        print(f"Downloading - {url}")
-        with open(f'/data/{model_name}.tar.gz', 'wb') as f:
-            r = requests.get(url)
-            f.write(r.content)
-        break
+model_name = os.environ.get('MODEL')
+
+if model_name:
+    url = "https://viso-public-storage.s3.eu-central-1.amazonaws.com/ObjDet/CPU/{}.tar.xz".format(model_name)
+    print(f"Downloading - {url}")
+    with open(f'/data/{model_name}.tar.gz', 'wb') as f:
+        r = requests.get(url)
+        f.write(r.content)
 print("Finished!")
